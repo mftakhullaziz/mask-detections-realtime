cc906bdc0232a24264bcb53c6b6c6d70d560e461
miftakhulaziz03 <azizmiftakhul19@gmail.com>
2021-02-19 23:37:43 +0700

    update images-mask-detection

--- images-mask-detection.py
+++ images-mask-detection.py
@@ -8,3 +8,16 @@ import cv2
 import os
 from plyer import notification
 
+def images_mask_detection :
+    ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True,
+		help="path to input image")
+	ap.add_argument("-f", "--face", type=str,
+		default="face_detector",
+		help="path to face detector model directory")
+	ap.add_argument("-m", "--model", type=str,
+		default="mask_detector.model",
+		help="path to trained face mask detector model")
+	ap.add_argument("-c", "--confidence", type=float, default=0.5,
+		help="minimum probability to filter weak detections")
+	args = vars(ap.parse_args())
