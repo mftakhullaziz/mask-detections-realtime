d71fce5d67f2d31b466e198772e9ae460d534093
miftakhulaziz03 <azizmiftakhul19@gmail.com>
2021-02-19 23:44:08 +0700

    update code

--- images-mask-detection.py
+++ images-mask-detection.py
@@ -49,3 +49,12 @@ def images_mask_detection :
         confidence = detections[0, 0, i, 2]
         if confidence>args["confidence"]:
             box = detections[0, 0, i, 3:7]*np.array([w,h,w,h])
+            (startX, startY, endX, endY) = box.astype("int")
+            (startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            face = image[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+			face = np.expand_dims(face, axis=0)
